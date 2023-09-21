b7100924e4895b6d0a2b4198c617d5124a7b0946
Koray Kutanoğlu <koraykutan53@gmail.com>
2023-09-21 23:53:55 +0300

    kubectl desteği eklendi

--- Scripts/Kubernetes/rke2-server-create.sh
+++ Scripts/Kubernetes/rke2-server-create.sh
@@ -10,7 +10,10 @@ source ../main.sh
 # create_server "$server_name" "$server_image" "$server_type" "$server_location" "$server_sshkey"
 create_server "k3s" "ubuntu-20.04" "cx21" "hel1" "mac"
 
-echo "uzak sunucuya komut gönderiliyor"
-ssh root@$ip "sudo -E apt update;sudo -E apt upgrade -y;curl -sfL https://get.k3s.io | sudo sh -;kubectl get nodes"
+echo "${separator// /-} Install RKE2 ${separator// /-}"
+ssh root@$ip "curl -sfL https://get.rke2.io | sudo sh -;sudo systemctl start rke2-server.service;"
+
+echo "${separator// /-} Install Kubectl And Configuration ${separator// /-}"
+ssh root@$ip "curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl";sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl;chmod +x kubectl;mkdir -p ~/.local/bin/kubectl;mv ./kubectl ~/.local/bin/kubectl;source /usr/share/bash-completion/bash_completion;echo 'source <(kubectl completion bash)' >>~/.bashrc;kubectl completion bash >/etc/bash_completion.d/kubectl;echo 'alias k=kubectl' >>~/.bashrc;echo 'complete -F __start_kubectl k' >>~/.bashrc;mkdir -p .kube;cp /etc/rancher/rke2/rke2.yaml /root/.kube/config;chmod 400 .kube/config"
 
 delete_server
