9e5b43265e06df5ca24f936d2ddf17d201d1558b
Koray Kutanoğlu <koraykutan53@gmail.com>
2023-09-21 22:17:20 +0300

    boş makine oluşturmak için script eklendi

--- Scripts/empty-machine-create.sh
+++ Scripts/empty-machine-create.sh
@@ -8,7 +8,7 @@
 # cpx31 =  4 CPU 8GB Ram
 
 echo "machine oluşturuluyor"
-ip=$(hcloud server create --name k3s --image ubuntu-20.04 --type cx21 --without-ipv6 --location hel1 --ssh-key mac | grep "IPv4:" | awk '{print $2}')
+ip=$(hcloud server create --name k3s --image ubuntu-20.04 --type cpx31 --without-ipv6 --location hel1 --ssh-key mac | grep "IPv4:" | awk '{print $2}')
 echo "Oluşturulan IP: $ip"
 
 while ! nc -z $ip 22; do
@@ -19,8 +19,8 @@ done
 echo "Sunucu known_hosts'a ekleniyor"
 ssh-keyscan -H "$ip" -p 22 >> /Users/koraykutanoglu/.ssh/known_hosts
 
-echo "uzak sunucuya komut gönderiliyor"
-ssh root@$ip "sudo -E apt update;sudo -E apt upgrade -y;curl -sfL https://get.k3s.io | sudo sh -;kubectl get nodes"
+echo "Güncellemeler Yapılıyor"
+ssh root@$ip "sudo -E apt update;sudo -E apt upgrade -y"
 
 echo "sunucuya bağlanılıyor"
 ssh root@$ip
