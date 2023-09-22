2fb5a3b7f668ce9500769b288ebc7525ac519fbf
Koray Kutanoğlu <koraykutan53@gmail.com>
2023-09-22 22:37:31 +0300

    Installation script for nodejs has been added.

---
+++ Scripts/Software/NodeJS/nodejs-latest-version-server-create.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###################### NODEJS INSTALL WITH NODESOURCE (VERSION 20.X) ######################
+
+source ../main.sh 
+
+# cx21  =  2 CPU 4GB Ram
+# cpx21 =  3 CPU 4GB Ram
+# cx31  =  2 CPU 8GB Ram
+# cpx31 =  4 CPU 8GB Ram
+
+SERVER_NAME=ubuntu-Latest-NodeJS-Server
+SERVER_IMAGE=ubuntu-22.04
+SERVER_TYPE=cx21
+SERVER_LOCATION=hel1
+SERVER_SSHKEY=mac
+
+create_server "$SERVER_NAME" "$SERVER_IMAGE" "$SERVER_TYPE" "$SERVER_LOCATION" "$SERVER_SSHKEY"
+
+echo "${separator// /-} Install NodeJS And npm ${separator// /-}"
+ssh root@$ip <<EOF
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_\$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo -E apt-get install -y nodejs
+EOF
+
+delete_server "$SERVER_NAME"
--- Scripts/Software/NodeJS/nodejs-server-create.sh
+++ Scripts/Software/NodeJS/nodejs-server-create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+###################### NODEJS INSTALL WITH APT ######################
+
 source ../main.sh 
 
 # cx21  =  2 CPU 4GB Ram
@@ -7,12 +9,18 @@ source ../main.sh
 # cx31  =  2 CPU 8GB Ram
 # cpx31 =  4 CPU 8GB Ram
 
-SERVER_NAME=ubuntu-22-04
+SERVER_NAME=ubuntu-NodeJS-Server
 SERVER_IMAGE=ubuntu-22.04
 SERVER_TYPE=cx21
 SERVER_LOCATION=hel1
 SERVER_SSHKEY=mac
 
 create_server "$SERVER_NAME" "$SERVER_IMAGE" "$SERVER_TYPE" "$SERVER_LOCATION" "$SERVER_SSHKEY"
+
+echo "${separator// /-} Install NodeJS ${separator// /-}"
+ssh root@$ip "apt install nodejs -y"
+
+echo "${separator// /-} Install npm ${separator// /-}"
+ssh root@$ip "apt install npm -y"
 
 delete_server "$SERVER_NAME"
