7a677fda44ac2133ab6a2b2f563a455933b2cf98
Koray Kutanoğlu <koraykutan53@gmail.com>
2023-09-27 14:01:52 +0300

    gaps organised

--- Scripts/Kubernetes/k3s-cluster-create.sh
+++ Scripts/Kubernetes/k3s-cluster-create.sh
@@ -17,8 +17,8 @@ create_server "$SERVER_NAME" "$SERVER_IMAGE" "$SERVER_TYPE" "$SERVER_LOCATION" "
 
 echo "${separator// /-} Install K3S ${separator// /-}"
 ssh root@$ip <<EOF
-curl -sfL https://get.k3s.io | sudo sh -
-kubectl get nodes
+  curl -sfL https://get.k3s.io | sudo sh -
+  kubectl get nodes
 EOF
 
 echo "${separator// /-} Get Agent Token ${separator// /-}" 
@@ -40,14 +40,11 @@ ssh-keyscan -H "$worker1ip" -p 22 >> /Users/koraykutanoglu/.ssh/known_hosts
 
 echo "${separator// /-} Server Update & Upgrade Process ${separator// /-}"
 ssh root@$worker1ip <<EOF
-sudo -E apt update
-sudo -E apt upgrade -y
-curl -sfL https://get.k3s.io | K3S_URL=https://$ip:6443 K3S_TOKEN="$token" sh -
-systemctl enable --now k3s-agent
+  sudo -E apt update
+  sudo -E apt upgrade -y
+  curl -sfL https://get.k3s.io | K3S_URL=https://$ip:6443 K3S_TOKEN="$token" sh -
+  systemctl enable --now k3s-agent
 EOF
-
-
-
 
 echo "${separator// /-} Creating Kubernetes Agent ${separator// /-}"  
 worker02ip=$(hcloud server create --name k3s-worker02 --image $SERVER_IMAGE --type $SERVER_TYPE --without-ipv6 --location $SERVER_LOCATION --ssh-key $SERVER_SSHKEY | grep "IPv4:" | awk '{print $2}')
@@ -64,26 +61,25 @@ ssh-keyscan -H "$worker02ip" -p 22 >> /Users/koraykutanoglu/.ssh/known_hosts
 
 echo "${separator// /-} Server Update & Upgrade Process ${separator// /-}"
 ssh root@$worker02ip <<EOF
-sudo -E apt update
-sudo -E apt upgrade -y
-curl -sfL https://get.k3s.io | K3S_URL=https://$ip:6443 K3S_TOKEN="$token" sh -
-systemctl enable --now k3s-agent
+  sudo -E apt update
+  sudo -E apt upgrade -y
+  curl -sfL https://get.k3s.io | K3S_URL=https://$ip:6443 K3S_TOKEN="$token" sh -
+  systemctl enable --now k3s-agent
 EOF
 
 
 echo "${separator// /-} Set Label Kubernetes Nodes ${separator// /-}"
 ssh root@$ip <<EOF
-kubectl label nodes k3s-worker01 node-role.kubernetes.io/master=true
-kubectl label nodes k3s-worker01 node-role.kubernetes.io/worker=true
-kubectl label nodes k3s-worker01 node-role.kubernetes.io/control-plane=true
+  kubectl label nodes k3s-worker01 node-role.kubernetes.io/master=true
+  kubectl label nodes k3s-worker01 node-role.kubernetes.io/worker=true
+  kubectl label nodes k3s-worker01 node-role.kubernetes.io/control-plane=true
 
-kubectl label nodes k3s-worker02 node-role.kubernetes.io/master=true
-kubectl label nodes k3s-worker02 node-role.kubernetes.io/worker=true
-kubectl label nodes k3s-worker02 node-role.kubernetes.io/control-plane=true
+  kubectl label nodes k3s-worker02 node-role.kubernetes.io/master=true
+  kubectl label nodes k3s-worker02 node-role.kubernetes.io/worker=true
+  kubectl label nodes k3s-worker02 node-role.kubernetes.io/control-plane=true
 
-kubectl label nodes k3s-main node-role.kubernetes.io/worker=true
+  kubectl label nodes k3s-main node-role.kubernetes.io/worker=true
 EOF
-
 
 delete_server "$SERVER_NAME"
 
